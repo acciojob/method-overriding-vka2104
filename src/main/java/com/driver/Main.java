81c16260aa813b8abdbca7f3d4cbd9411159d19e
anjana <anjana123@gmail.com>
2023-08-25 23:45:29 +0530

    Fixed the method override task

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,24 @@
 package com.driver;
 
 public class Main {
+    int a  = 0;
+    public static class A {
+        public A() {
+        }
+
+        public String meth() {
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A{
+        public B() {
+        }
+
+        @Override
+        public String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+    }
   public static void main(String args[]) {
       B b = new B();
       System.out.println(b.meth());
